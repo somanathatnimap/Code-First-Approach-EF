ee2268ddfe255af7fecb2abc68d56e8fc2b303da
Somanath Barge <somanath@nimap.com>
2023-07-11 16:39:45 +0530

    Add project files.

---
+++ EFCodeFirstApproch/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCodeFirstApproch.Models;
+
+namespace EFCodeFirstApproch.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentContext db=new StudentContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data=db.Students.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ EFCodeFirstApproch/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EFCodeFirstApproch.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; } 
+        public int Age { get; set; }
+
+
+    }
+}
