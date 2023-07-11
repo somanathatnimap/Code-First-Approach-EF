4490a2e37c62025f18f2728323f0261e7fc09b52
Somanath Barge <somanath@nimap.com>
2023-07-11 16:48:09 +0530

    Added product models and controller

---
+++ EFCodeFirstApproch/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFCodeFirstApproch.Controllers
+{
+    public class ProductController
+    {
+        // method 1
+
+        // method 2
+    }
+}
